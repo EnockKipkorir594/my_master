b313950f508923d39eae7f7ccc2d265f69610b27
Enock <kipkorirenock594@gmail.com>
2026-04-28 11:30:47 +0300

    Changed POST to PUT since it was update an item not creating one.

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ def delete(id):
     except:
         return 'The task could not be deleted'
     
-@app.route('/update/<int:id>', methods=['GET','POST'])
+@app.route('/update/<int:id>', methods=['GET','PUT'])
 def update(id):
     task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
+    if request.method == 'PUT':
         task.content = request.form['content']
 
         try:
